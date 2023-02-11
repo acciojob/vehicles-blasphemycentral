9de23aed6b2adb82a69da2eefc80160efdebf298
blasphemycentral <aayushnag84@gmail.com>
2023-02-11 16:39:55 +0530

    solution 1

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,18 @@
+package com.driver;
+
+public class Boat implements WaterVehicle
+{
+    String name;
+    int capacity;
+    @Override
+    public String getVehicleName()
+    {
+        return this.name;
+    }
+
+    @Override
+    public int getVehicleCapacity()
+    {
+        return this.capacity;
+    }
+}
--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -4,22 +4,38 @@ public class Car extends Vehicle {
     private int wheels;
     private String type;
     private int doors;
-    private int gears;
-    private boolean isManual;
-    private int currentGear;
+    public int gears;
+    public boolean isManual;
+    public int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats)
+    {
+        super.name = name;
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.currentGear = 1;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
         //Hint: Car extends Vehicle
     }
+    public Car()
+    {
 
-    public void changeGear(int newGear){
+    }
 
+    public void changeGear(int newGear)
+    {
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
-
+    public void changeSpeed(int newSpeed, int newDirection)
+    {
+        super.currentSpeed = newSpeed;
+        super.currentDirection = newDirection;
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
